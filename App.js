ceb2a24c80e901becefc55820d7245e7448cc3cb
Roshan <roshanyadav1793@gmail.com>
2021-03-08 14:41:41 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Home from './components/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="header">
+        Hello world
+      </header>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './styles/Navbar.css';
+
+const Navbar = () => {
+    return(
+        <>
+            <div className="Nav">
+                            
+            </div>
+        </>
+    );
+}
+
+export default Navbar;
