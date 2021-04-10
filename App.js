864bf48235e75bf60ec18c6818fa38dbccb073da
Roshan <roshanyadav1793@gmail.com>
2021-04-10 21:25:49 +0530

    footer

--- App.js
+++ App.js
@@ -6,7 +6,6 @@ function App() {
   return (
     <div className="App">
       <header className="header">
-        Hello world
       </header>
       <Home />
     </div>
--- components/Home.js
+++ components/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Navbar from './Navbar';
-
+import Footer from './Footer/Footer';
 function Home() {
     return(
         <div>
             <Navbar />
+            <Footer />
         </div>
     );
 }
