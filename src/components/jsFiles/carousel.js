a429dd403c932791acc50455c4e6b885f7f640ff
Utkarsh Sharma <62636987+utkarsh3112@users.noreply.github.com>
2021-04-13 00:13:21 +0530

    Update carousel.js

--- src/components/jsFiles/carousel.js
+++ src/components/jsFiles/carousel.js
@@ -4,39 +4,16 @@ import img1 from '../images/img1.jpg';
 import img2 from '../images/img2.jpg';
 import img3 from '../images/img3.jpg';
 import img4 from '../images/img4.jpg';
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import Slider from 'react-slick';
 import './carousel.css';
 
+
 export default function SimpleSlider() {
-    var settings = {
-      dots: true,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 1,
-      Animation:1,
-      slidesToScroll: 1
-    };
-    return (
-        <div id="Ca_main">
-            <div id="Ca_main_section1">
-            <Slider {...settings}>
-                <div>
-                    <img src={img1} id="Ca_img" />
-                </div>
-                <div>
-                    <img src={img2} id="Ca_img" />
-                </div>
-                <div>
-                    <img src={img3} id="Ca_img" />
-                </div>
-                <div>
-                    <img src={img1} id="Ca_img" />
-                </div>                
-            </Slider>
+    return(
+        <div id="carousel_main">
+            <div className="main_top_carousel" >
+      
             </div>
         </div>
-     
+        
     );
   }
