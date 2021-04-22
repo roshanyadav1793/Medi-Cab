0e5f375ba8dfbe3366381b8452603fb13fb01934
Roshan <roshanyadav1793@gmail.com>
2021-04-22 11:56:45 +0530

    aboutus page

--- src/components/Aboutus/Aboutus.js
+++ src/components/Aboutus/Aboutus.js
@@ -1,27 +1,38 @@
 import React from 'react';
-import styles from './styles/Aboutus.module.css';
+import classes from './styles/Aboutus.module.css';
 import BubbleChartIcon from '@material-ui/icons/BubbleChart';
 import Navbar from '../jsFiles/Navbar';
 import Footer from '../Footer/Footer';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import InstagramIcon from '@material-ui/icons/Instagram';
 
 const aboutUs=()=>{
     return (
         <>  
             <Navbar />
-            <div className={styles.about_page}>
-                <h1 className={styles.h1}><BubbleChartIcon/> About Us <BubbleChartIcon/> </h1>
-                <hr></hr>
-                <section className={styles.section}>
-                    <p>Welcome to MedQ! The purpose of your service is to create our MedQ. So we always want to be by your side....</p>
-                    
-                </section>
-                <h2 className={styles.h2}>Features</h2>
-                <ul>
-                    <li>High Production</li>
-                    <li>Reliable supplying</li>
-                    <li>Quick Delivery</li>
-                </ul>
-            </div>
+            <section>
+                <div className={classes.aboutus}>
+                    <h1>About Us</h1>
+                    <div className={classes.wrapper}>
+                        <div className={classes.content}>
+                            <h3>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy</h3>
+                            <p>t is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved.</p>
+                            <div className={classes.button}>
+                                <a href="#">read more</a>
+                            </div>
+                            <div className="sci">
+                                <a href="#"><FacebookIcon /></a>
+                                <a href="#"><TwitterIcon /></a>
+                                <a href="#"><InstagramIcon /></a>
+                            </div>
+                        </div>
+                        <div className={classes.imageSection}>
+                            <img src="https://i.ytimg.com/vi/EAOIKUASGDE/maxresdefault.jpg" />
+                        </div>
+                    </div>
+                </div>
+            </section>
             <Footer />
         </>
     );
